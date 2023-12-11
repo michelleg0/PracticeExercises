476eb23dc49cc603ff89f73b9f816f5d5995aec0
Michelle Guerrero <michelleguerrero@Michelles-MacBook-Pro-2.local>
2023-12-10 23:45:06 -0800

    first commit

---
+++ activity1/Activity.java
@@ -0,0 +1,47 @@
+package activity1;
+
+public class Activity {
+    // NOTE: DO NOT google solutions. Complete each part on your own.
+
+    // This exercise creates basic functions of a Calculator.
+    // Run tests in ActivityTest file. All tests should fail.
+    // Complete each method below. After completing it, re-run the tests.
+    // If everything was solved correctly, the tests should pass.
+    public int sum(int a, int b) {
+        // TODO 1 - this should return the sum of two numbers.
+        a = 1;
+        b = 2;
+        System.out.println(a+b);
+
+        return -1;
+    }
+
+    public int subtract(int a, int b) {
+        // TODO 1 - this should return the difference of two numbers.
+        a = 3;
+        b = 1;
+        System.out.println(a-b);
+
+        return -1;
+
+    }
+
+    public int multiply(int a, int b) {
+        /* TODO 1 - this should return the product of two numbers. */
+        a = 3;
+        b = 2;
+        System.out.println(a*b);
+
+        return -1;
+
+    }
+
+    public int divide(int a, int b) {
+        // TODO 1 - this should return the division of two numbers.
+        a = 4;
+        b = 2;
+        System.out.println(a/b);
+
+        return -1;
+    }
+}
---
+++ activity3/Activity.java
@@ -0,0 +1,21 @@
+package activity3;
+
+public class Activity {
+    // NOTE: DO NOT google solutions. Complete each part on your own.
+
+    // This exercise determines if a number is Even or Odd.
+    // Run tests in ActivityTest file. All tests should fail.
+    // Complete each method below. After completing it, re-run the tests.
+    // If everything was solved correctly, the tests should pass.
+    public String oddOrEven(int number) {
+        // TODO 1 - this should return the word "even" or "odd" based on what the number is.
+        number = 4;
+            if ( number % 2 == 0) {
+                System.out.println("This number is even.");
+            } else {
+                System.out.println("This number is odd.");
+            }
+
+        return null;
+    }
+}
